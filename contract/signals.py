ff6d79a4b4b75aefbdcffe23e2824c459fc969e5
iamoracle <officialbilas@gmail.com>
2022-06-05 12:14:26 +0100

    :sparkles: send email to trustee
    send email to depositors and trustees

---
+++ contract/signals.py
@@ -0,0 +1,82 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+
+from . import models
+
+
+@receiver(post_save, sender=models.DepositorModel)
+def email_payment_request_to_depositor(sender, instance, created, *args, **kwargs):
+
+    if created:
+    
+        # send an e-mail to the user
+
+        user: models.UserModel = instance.user
+
+        context = {
+            'username': user.username,
+            'email': user.email,
+            'amount': instance.amount,
+            'wallet_address' : instance.wallet_address,
+            'contract_id': instance.contract.id,
+        }
+
+        # render email text
+        email_html_message = render_to_string(
+            'contract/sample.html', context)
+        email_plaintext_message = render_to_string(
+            'contract/sample.txt', context)
+
+        msg = EmailMultiAlternatives(
+            # title:
+            f"Someone added you as a Depositor",
+            # message:
+            email_plaintext_message,
+            # from:
+            settings.EMAIL_SENDER,
+            # to:
+            [user.email]
+        )
+        msg.attach_alternative(email_html_message, "text/html")
+        msg.send()
+        
+        
+@receiver(post_save, sender=models.TrusteeModel)
+def email_payment_request_to_trustee(sender, instance, created, *args, **kwargs):
+
+    if created:
+    
+        # send an e-mail to the user
+
+        user: models.UserModel = instance.user
+
+        context = {
+            'username': user.username,
+            'email': user.email,
+            'amount': instance.amount,
+            'wallet_address' : instance.wallet_address,
+            'contract_id': instance.contract.id,
+        }
+
+        # render email text
+        email_html_message = render_to_string(
+            'contract/sample.html', context)
+        email_plaintext_message = render_to_string(
+            'contract/sample.txt', context)
+
+        msg = EmailMultiAlternatives(
+            # title:
+            f"Someone Added you as a Trustee",
+            # message:
+            email_plaintext_message,
+            # from:
+            settings.EMAIL_SENDER,
+            # to:
+            [user.email]
+        )
+        msg.attach_alternative(email_html_message, "text/html")
+        msg.send()
