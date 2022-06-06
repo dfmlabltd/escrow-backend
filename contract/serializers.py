f3ece46eb217a79a88a478250d5e18a1ee07c644
iamoracle <officialbilas@gmail.com>
2022-06-06 16:10:07 +0100

    :ambulance: fixed all contract create bugs
    fixed all contract bugs

--- contract/serializers.py
+++ contract/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from . import models
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils.encoding import smart_str
+from typing import List, Dict
+from django.utils.translation import ugettext_lazy as _
 
 
 class CreatableSlugRelatedField(serializers.SlugRelatedField):
@@ -51,30 +53,65 @@ class ContractSerializer(serializers.ModelSerializer):
 
     depositors = DepositorSerializer(many=True)
 
-    trustees = TrusteeSerializer(many=True)
+    trustees = TrusteeSerializer(many=True, allow_empty=False)
 
     read_only_fields = ('contract_address', 'time_created',
                         'time_update', 'status')
 
     def create(self, validated_data):
+        """"
+        1. removes depositors & trustee from dict
+        2. loop through to add all depositors & trustees amount
+        3. optimize by caching in a List (do not hit db)
+        4. check if amounts are equal (total deposit == total withdrawal)
+        """
 
-        depositors = validated_data.pop('depositors', [])
+        depositors: Dict = validated_data.pop('depositors', [])
 
-        trustees = validated_data.pop("trustees", [])
+        trustees: Dict = validated_data.pop("trustees", [])
 
-        contract = models.ContractModel.objects.create(**validated_data)
+        contract: models.ContractModel = models.ContractModel(
+            **validated_data)
+
+        depositors_model: List[models.DepositorModel] = []
+
+        total_deposit_amount = 0
 
         for depositor in depositors:
 
+            total_deposit_amount += depositor['amount']
+
             depositor['contract'] = contract
 
-            models.DepositorModel.objects.create(**depositor)
+            depositors_model.append(models.DepositorModel(**depositor))
+
+        else:
+
+            total_deposit_amount = contract.amount
+
+        total_trustee_amount = 0
+
+        trustees_model: List[models.TrusteeModel] = []
 
         for trustee in trustees:
 
+            total_trustee_amount += trustee['amount']
+
             trustee['contract'] = contract
 
-            models.TrusteeModel.objects.create(**trustee)
+            trustees_model.append(models.TrusteeModel(**trustee))
+
+        if not (total_trustee_amount == total_deposit_amount and
+                total_deposit_amount == contract.amount):
+
+            raise serializers.ValidationError(
+                {_("detail"): _('total amount not equal')})
+
+        contract.save()
+
+        models.DepositorModel.objects.bulk_create(depositors_model)
+
+        models.TrusteeModel.objects.bulk_create(trustees_model)
 
         return contract
 
