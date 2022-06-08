3bec0771ee47228d56e848595be9ee21a4dedd64
iamoracle <officialbilas@gmail.com>
2022-06-08 16:45:11 +0100

    :green_heart: fixed nginx and docker
    fixed nginx and docker

--- escrow/settings.py
+++ escrow/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['api.adehun.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['api.adehun.com', '18.217.246.202']
 
 
 # Application definition
