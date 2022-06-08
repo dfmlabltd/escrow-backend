d41b7b1fbad3bf69b9f000d311f21a8c574dce61
iamoracle <officialbilas@gmail.com>
2022-06-08 11:42:30 +0100

    :heavy_plus_sign: installed corsheader
    installed corsheader

--- escrow/settings.py
+++ escrow/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    'corsheaders',
     'rest_framework',
     'user',
     'contract',
@@ -58,6 +59,7 @@ INSTALLED_APPS += [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
