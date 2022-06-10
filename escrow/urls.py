994bafdb599e622a0c62023359d3538a06d453cd
iamoracle <officialbilas@gmail.com>
2022-06-11 00:00:43 +0100

    :bug: added endpoint url
    added enpoint url

--- escrow/urls.py
+++ escrow/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('dashboard/', admin.site.urls),
     path('user/', include('user.urls')),
     path('contract/', include('contract.urls')),
+    path('crypto/', include('coin.urls')),
 ]
