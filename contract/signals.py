01e3be665766105b91ae40a94ff305f5c8f70679
iamoracle <officialbilas@gmail.com>
2022-06-23 00:48:15 +0100

    :poop: commented signals

--- contract/signals.py
+++ contract/signals.py
@@ -51,41 +51,41 @@ def email_payment_request_to_depositor(sender, instance, created, *args, **kwarg
         msg.send()
         
         
-@receiver(post_save, sender=models.TrusteeModel)
-def email_payment_request_to_trustee(sender, instance, created, *args, **kwargs):
+# @receiver(post_save, sender=models.TrusteeModel)
+# def email_payment_request_to_trustee(sender, instance, created, *args, **kwargs):
     
-    logger.error("user.email")
-    logger.error(created)
+#     logger.error("user.email")
+#     logger.error(created)
 
-    if created:
+#     if created:
     
-        # send an e-mail to the user
+#         # send an e-mail to the user
 
-        user: models.UserModel = instance.user
+#         user: models.UserModel = instance.user
 
-        context = {
-            'username': user.username,
-            'email': user.email,
-            'amount': instance.amount,
-            'wallet_address' : instance.wallet_address,
-            'contract_id': instance.contract.id,
-        }
+#         context = {
+#             'username': user.username,
+#             'email': user.email,
+#             'amount': instance.amount,
+#             'wallet_address' : instance.wallet_address,
+#             'contract_id': instance.contract.id,
+#         }
 
-        # render email text
-        email_html_message = render_to_string(
-            'contract/base.html', context)
-        email_plaintext_message = render_to_string(
-            'contract/base.txt', context)
+#         # render email text
+#         email_html_message = render_to_string(
+#             'contract/base.html', context)
+#         email_plaintext_message = render_to_string(
+#             'contract/base.txt', context)
 
-        msg = EmailMultiAlternatives(
-            # title:
-            f"Someone Added you as a Trustee",
-            # message:
-            email_plaintext_message,
-            # from:
-            settings.EMAIL_SENDER,
-            # to:
-            [user.email]
-        )
-        msg.attach_alternative(email_html_message, "text/html")
-        msg.send()
+#         msg = EmailMultiAlternatives(
+#             # title:
+#             f"Someone Added you as a Trustee",
+#             # message:
+#             email_plaintext_message,
+#             # from:
+#             settings.EMAIL_SENDER,
+#             # to:
+#             [user.email]
+#         )
+#         msg.attach_alternative(email_html_message, "text/html")
+#         msg.send()
